6ad211a8077176f1eacc373901cdd62bba185c05
albertKengerli <50487338+albertKengerli@users.noreply.github.com>
2019-10-27 21:33:17 +0300

    feat: implement js for buttons to change preset canvas

--- codejam-canvas/main.js
+++ codejam-canvas/main.js
@@ -1,18 +1,19 @@
 let url = "https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json";
 let url2 = "https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/32x32.json";
+let url3 ="https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/image.png";
+
+let image = new Image;
+image.src = url3;
+console.log(image);
 
 let canvas = document.getElementById("canvas");
+let button4x4 = document.getElementById("button4x4");
+let button32x32 = document.getElementById("button32x32");
+let button256x256 = document.getElementById("button256x256");
+let buttonBlank = document.getElementById("buttonBlank");
+
 let drawing = canvas.getContext('2d');
-
 let canvasSize = document.getElementById("canvas").clientWidth;
-let config2 = [
-  ["00BCD4", "FFEB3B","FFEB3B","00BCD4"],
-  ["FFEB3B", "FFC107","FFC107","FFEB3B"],
-  ["FFEB3B", "FFC107","FFC107","FFEB3B"],
-  ["00BCD4", "FFEB3B","FFEB3B","00BCD4"]
-];
-
-//fillRect + fillStyle
 
 function readConfig(url) {
   return fetch(url)
@@ -36,5 +37,20 @@ function canvasColor(color){
   } else return "rgba("+color.join(",")+")";
 }
 
-readConfig(url)
-  .then(result => draw(result));
+button4x4.addEventListener("click", function(){
+  readConfig(url)
+    .then(result => draw(result));
+});
+
+button32x32.addEventListener("click", function(){
+  readConfig(url2)
+    .then(result => draw(result));
+});
+
+button256x256.addEventListener("click", function(){
+  drawing.drawImage(image, 0, 0, canvasSize, canvasSize);
+});
+
+buttonBlank.addEventListener("click", function(){
+  drawing.clearRect(0, 0, canvasSize, canvasSize);
+});
